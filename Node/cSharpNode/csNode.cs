0d7884c51c998c91423ad7d69279a24ea5748de9
Tim <owner@Owners-MacBook-Pro.local>
2017-12-25 15:18:55 -0500

    small changes

--- Node/cSharpNode/csNode.cs
+++ Node/cSharpNode/csNode.cs
@@ -2,11 +2,25 @@
 
 namespace cSharpNode
 {
-    class csNode
+   public class csNode
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+		private object data;
+		private csNode next;
+
+        //constructor for the node
+        public csNode(csNode next, object data){
+            this.data = data;
+            this.next = next;
         }
+
+        //properties for data
+        public object Data{
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+
+
+
     }
 }
