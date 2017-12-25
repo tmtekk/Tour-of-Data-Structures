753522388e246f79d7f86efbd5f24ca370fde887
Tim <owner@Owners-MacBook-Pro.local>
2017-12-25 14:33:52 -0500

    changes to python node class

---
+++ Node/pythonNode.py
@@ -0,0 +1,16 @@
+
+class Node(object):
+    """docstring for Node."""
+    def __init__(self, data, nextNode=None):
+        self.data = data
+        self.next = nextNode
+
+    def getData(self, data):
+        return self.data
+    def getNext(self,  nextNode):
+        return self.nextNode
+
+    def setData(self, data):
+        self.data = data
+    def setNext(self, nextNode):
+        self.nextNode = nextNode
